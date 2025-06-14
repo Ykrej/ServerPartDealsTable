6c86e6d02fc8007fc66ddd8aac3ec1a6788d1310
Ykrej <stellan125@gmail.com>
2025-06-14 14:04:02 -0700

    fix: formatting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,11 @@
 
     let { data } = $props()
     console.log("Item Count", data.serverPartsData.length)
-    
+
     const columns = ["brand", "capacityGb", "condition", "formFactor", "type", "priceUsd", "sku", "interface", "interfaceSpeed"].map(field => ({
         field,
         filter: true
     }))
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-<h1>Data</h1>
 <Grid columnDefs={columns} rowData={data.serverPartsData} />
