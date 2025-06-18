5db9312857eddd733b36206aa665d3b7b4fa6b04
Ykrej <stellan125@gmail.com>
2025-06-17 19:00:16 -0700

    feat: load solid state and hard drives on page load

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,9 +5,13 @@ import {
 import type { PageLoad } from './$types'
 
 export const load: PageLoad = async () => {
+  const serverPartsData = (
+    await Promise.all([
+      getServerPartsRecords(ServerPartsCollection.HARD_DRIVES),
+      getServerPartsRecords(ServerPartsCollection.SOLID_STATE_DRIVES),
+    ])
+  ).flatMap((record) => record)
   return {
-    serverPartsData: await getServerPartsRecords(
-      ServerPartsCollection.HARD_DRIVES
-    ),
+    serverPartsData,
   }
 }
